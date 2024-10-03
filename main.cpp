8107386753da215935ca683e2c45cbcabd113e64
Anton Pashenyuk <anton.mgdn@gmail.com>
2024-10-03 13:55:51 +0700

    forwardlist added to project

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Header.h"
+using namespace std;
+
+struct NodeLL
+{
+	string value;
+	NodeLL* previousNode;
+	NodeLL* nextNode;
+};
+
+struct LinkedList
+{
+private:
+	NodeLL* tail;
+	NodeLL* head;
+public:
+
+};
+
+
+int main()
+{
+	
+	return 0;
+}
